bd961e816556090e1d2098a267daf6d6d78f1572
YuanyuanChen8 <90532924+YuanyuanChen8@users.noreply.github.com>
2021-10-01 00:37:44 -0700

    Update question 1.java

--- question 1.java
+++ question 1.java
@@ -5,15 +5,21 @@ public class Main {
     public Main(){
 
     }
-    static int res=Integer.MAX_VALUE;
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
-        int[] nums=new int[]{1,2};
-        //int[] x=new int[]{2,1,6,4,8};
-        //int[] y=new int[]{2,3,5,4};
-        int ans =sumOfUnique(nums);
-        System.out.println(ans);
+    public static void main(String[] args) {
+
+        int[] nums1=new int[]{1,2,3,2};
+        int ans1 =sumOfUnique(nums1);
+        System.out.println(ans1);
+
+        int[] nums2=new int[]{1,1,1,1};
+        int ans2 =sumOfUnique(nums2);
+        System.out.println(ans2);
+
+        int[] nums3=new int[]{1,2,3,4,5};
+        int ans3 =sumOfUnique(nums3);
+        System.out.println(ans3);
+
 
     }
     public static int sumOfUnique(int[] nums) {
