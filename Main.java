bb337d71041ab0d35c94e014c62e4182b530d31c
YuanyuanChen8 <90532924+YuanyuanChen8@users.noreply.github.com>
2021-10-01 00:42:44 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String inputOne="1A3d4s5t";
+        boolean resultOne = example(inputOne);
+        System.out.println(resultOne);
+        String inputTwo="A2bb2d4";
+        boolean resultTwo = example(inputTwo);
+        System.out.println(resultTwo);
+    }
+    private static boolean example(String Result) {
+        boolean m = true;
+        boolean n = true;
+        for (int i = 0; i < Result.length() - 1; i++) {
+            m = Character.isDigit(Result.charAt(i));
+            n = Character.isDigit(Result.charAt(i + 1));
+            if (m == n){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
